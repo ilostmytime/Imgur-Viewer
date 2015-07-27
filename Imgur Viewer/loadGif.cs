526c1938c05d37a6f7bd5fed0baf6653ff189bd2
ilostmytoonie <ilostmytoonie@gmail.com>
2015-07-26 21:26:42 -0400

    Added some changes

---
+++ Imgur Viewer/loadGif.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Imgur_Viewer
+{
+    class loadGif
+    {
+        public loadGif(string url)
+        {
+            BitmapImage gif = new BitmapImage();
+            gif.BeginInit();
+            gif.UriSource = new Uri(url, UriKind.Absolute);
+            gif.EndInit();
+
+        }
+    }
+}
